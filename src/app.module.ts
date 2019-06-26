94615158712903fb62d51a608fb4c0e29eaf367b
Van Tran 🐞 <tmvan.1903@gmail.com>
2019-06-27 00:45:42 +0700

    init mvc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
+import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
 import ProductModule from './products/product.module';
 
 @Module({
   imports: [ProductModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
